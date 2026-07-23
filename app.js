b7b88c7e965f9c59921a9d927c29800e87dc2cf0
nocuadavid408-boop <nocuadavid408@gmail.com>
2026-07-23 17:11:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import "dotenv/config"
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", (_, res) => {
-  res.send(`Hola , estamos aprendiendo express con la ficha 3407184 ${port}`);
+  res.send(`Hola , estamos aprendiendo express con la ficha 3407184`);
 });
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
