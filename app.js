8b053031be844c6607190a9fcb692905ae5591da
nocuadavid408-boop <nocuadavid408@gmail.com>
2026-07-23 17:00:50 -0500

    entorno con variables puerto

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express');
+import express from 'express';
+import {configDotenv} from "dotenv"
+configDotenv();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get("/", (_, res) => {
-  res.send("Hola , estamos aprendiendo express con la ficha 3407184");
+  res.send(`Hola , estamos aprendiendo express con la ficha 3407184 ${port}`);
 });
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
