ab64ce16cce5fd4b5d25d7ff377e3917baa15399
jhonalexbeltran2008@gmail.com <nocuadavid408@gmail.com>
2026-07-30 13:40:14 -0500

    Nuevo ENDPOINT

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ import express from 'express';
 // import {configDotenv} from "dotenv"
 // configDotenv();
 import "dotenv/config" 
-
+import bodyparser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//configurar el uso de body-parser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
 app.get("/", (_, res) => {
   res.send(`Hola , estamos aprendiendo express con la ficha 3407184`);
 });
+
+//otro endopint, funcione de flecha
+app.get("/productos", (req,res) => {
+  //usando templates strings
+  res.send(`<h1>listado de productos</h1>
+    <ol>
+      <li>televisor</li>
+      <li>celular</li>
+      <li>impresora</li>
+    </ol>`);
+});
+
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
 });
