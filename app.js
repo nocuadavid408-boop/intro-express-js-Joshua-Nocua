43ce6ecd1ddb5bdc7becb6743b41164650916a42
nocuadavid408-boop <nocuadavid408@gmail.com>
2026-08-06 16:15:29 -0500

    cracion ruta uno, rama commonjs, control de versiones

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get("/", (_, res) => {
   res.send("Hola , estamos aprendiendo express con la ficha 3407184");
 });
 app.listen(port, () => {
   console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+});
+
+app.get("/datos", (_, res) => {
+  res.json({
+    datospersonales: {
+      nombre: "Joshua",
+      apellido: "Nocua",
+      lista_telefonos:[312567890, 
+        3123456789],
+    },
+    datosprograma: {
+      nombre_programa: "Analisis y Desarrollo de Software",
+      tipo_programa: "Tecnologo",
+      ficha: 3407184,
+    }
+  });
 });
