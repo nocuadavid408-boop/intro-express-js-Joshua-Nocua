c701fed08f1445db61cded56c0fcdb99decc85a5
nocuadavid408-boop <nocuadavid408@gmail.com>
2026-08-13 14:25:33 -0500

    Revert "lista aprendices."

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-import express from 'express';
-// import {configDotenv} from "dotenv"
-// configDotenv();
-import "dotenv/config" 
-import cors from "cors";
-import bodyparser from "body-parser";
-
+const express = require('express');
 const app = express();
 const port = 3000;
-app.get("/", (_, res) => {
+
+app.get("/", function(req, res) {
   res.send("Hola , estamos aprendiendo express con la ficha 3407184");
+})
+
+app.get("/aprendices", (req, res) => {
+  const listaAprendices = [
+    {"nombre": "Juan Camilo", 
+      "edad": 20, 
+      "correo": "juan.camilo@gmail.com",
+      "imgperfil": "url_img_1"
+    },
+    {"nombre": "Maria lucia", 
+      "edad": 22, 
+      "correo": "maria.lucia@gmail.com",
+      "imgperfil": "url_img_2"
+    },
+    {"nombre": "Carlos Andres", 
+      "edad": 21, 
+      "correo": "carlos.andres@gmail.com",
+      "imgperfil": "url_img_3"
+    }
+  ]
+  res.json(listaAprendices);
 });
-app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en el puerto: ${port}`);
-});
+
+app.listen(port, function() {
+  console.log(`SERVIDOR: http://localhost: ${port}`);
+})
